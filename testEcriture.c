8ef144b24a5fe9e7c42aba1da3c803ff94cb1784
Williams ARLAND ANYOU <warlandanyou@iut.univ-paris8.fr>
2018-10-16 09:32:27 +0200

    ajout lecture ecriture fichier

---
+++ testEcriture.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct caseTerrain
+{
+  int posX;
+  int posY;
+  int etat;
+  char  contenu;
+}caseTerrain;
+
+int main()
+{
+  FILE* f;
+  int taille;
+  int i, j;
+  printf("bonjour veuillez saisir la taille du terrain ?");
+  scanf("%d",&taille);
+  //creatTerrain(taille);
+  struct caseTerrain terrain[(taille*taille)+1];
+  
+  for(i=0;i<taille;i++){
+    for(j=0;j<taille;j++){
+        if (i==0 && j==0)
+            terrain[0].etat=taille;
+        
+      terrain[i+j].posX=i;
+      terrain[i+j].posY=j;
+      terrain[i+j].etat=0;
+        if(i%2==0)
+            terrain[i+j].contenu='V';
+        else
+            terrain[i+j].contenu='B';
+    }
+  }
+ f = fopen("terrainActuel.txt", "w");
+ if (f == NULL)
+   return 1;
+ fwrite(terrain, taille*taille , sizeof(struct caseTerrain), f);
+ fclose(f);
+ return 0;
+}
+
+/* creatTerrain(int t){
+ 
+ struct caseTerrain terrain[taille*taille];
+ 
+}
+*/
