1d09bfe0d63a05081033e2d73911a21b27efe4be
Williams ARLAND ANYOU <warlandanyou@iut.univ-paris8.fr>
2018-11-05 21:45:13 +0100

    Demineur terminee, essaye ajout graphique ??

--- CaseTerrain.c
+++ CaseTerrain.c
@@ -1,22 +1,105 @@
-//
-//  CaseTerrain.c
-//  
-//
-//  Created by Williams ARLAND on 17/10/2018.
-//
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
 
-#include "CaseTerrain.h"
-/*
-typedef struct caseTerrain
+#include "Demineur.h"
+
+void ouvertureCase(caseTerrain* terrain, int pX, int pY)
+/* Fonction qui va ouvrir un case, et si cette case est vide, va ouvrir ses cases voisines*/
 {
-    int posX;
-    int posY;
-    int etat;
-    int  contenu;
-}caseTerrain;
-
-int nbBombeVoisine(* terrain,struct caseTerrain){
-    int nb =0;
+    if ( (terrain+ (pX+pY*(*largeurTerrain) )) ->etat == C_FERMEE ){
+        (terrain+(pX+pY*(*largeurTerrain)) )->etat= C_OUVERTE;
+           if ( (terrain+(pX+pY*(*largeurTerrain)) )-> contenu == 0) {
+            ouvertureCaseVoisine(terrain, pX, pY);
+        }
+    }
+}
+void flagCase(caseTerrain* terrain, int pX, int pY)
+/* Fonction qui flag une case*/
+{
+    if ( (terrain+(pX+pY*(*largeurTerrain)) )->etat == C_FERMEE )
+        (terrain+(pX+pY*(*largeurTerrain)) )->etat= C_FLAG;
+    else if( (terrain+(pX+pY*(*largeurTerrain)) )->etat == C_FLAG )
+         (terrain+(pX+pY*(*largeurTerrain)) )->etat= C_FERMEE;
+}
+void ouvertureCaseVoisine(caseTerrain* terrain, int pX, int pY)
+/* Fonction qui va ouvrir les case voisines valide d'un case passee en parametre*/
+{
+    if(caseValide(pX,pY+1)){
+        ouvertureCase(terrain,pX,pY+1);
+    }
+    if(caseValide(pX+1,pY+1)){
+        ouvertureCase(terrain,pX+1,pY+1);
+    }
+    if(caseValide(pX+1,pY)){
+        ouvertureCase(terrain,pX+1,pY);
+    }
+    if(caseValide(pX+1,pY-1)){
+        ouvertureCase(terrain,pX+1,pY-1);
+    }
+    if(caseValide(pX,pY-1)){
+        ouvertureCase(terrain,pX,pY-1);
+    }
+    if(caseValide(pX-1,pY-1)){
+        ouvertureCase(terrain,pX-1,pY-1);
+    }
+    if(caseValide(pX-1,pY)){
+        ouvertureCase(terrain,pX-1,pY);
+    }
+    if(caseValide(pX-1,pY+1)){
+        ouvertureCase(terrain,pX-1,pY+1);
+    }
     
-    return nb;
-}*/
+}
+
+void ajoutNBombe(caseTerrain* terrain, int pX, int pY)
+/* Fonction qui verifie si la case est une bombe pour gerer ses voisines*/
+{
+    if ( (terrain+(pX+pY*(*largeurTerrain)) )->contenu == C_BOMBE ){
+        plus1CasesVoisines(terrain,pX, pY);
+    }
+}
+
+void plus1CasesVoisines(caseTerrain* terrain, int pX, int pY)
+/* Fonction qui increment le contenu des cases voisines d'un bombe */
+{
+    if(caseValide(pX,pY+1)){
+        if ( (terrain+(pX+(pY+1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+(pX+(pY+1)*(*largeurTerrain) ))->contenu +=1;
+    }
+    if(caseValide(pX+1,pY+1)){
+        if ( (terrain+((pX+1)+(pY+1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+((pX+1)+(pY+1)*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX+1,pY)){
+        if ( (terrain+((pX+1)+pY*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+((pX+1)+pY*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX+1,pY-1)){
+        if ( (terrain+((pX+1)+(pY-1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+((pX+1)+(pY-1)*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX,pY-1)){
+        if ( (terrain+(pX+(pY-1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+(pX+(pY-1)*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX-1,pY-1)){
+        if ( (terrain+((pX-1)+(pY-1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+((pX-1)+(pY-1)*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX-1,pY)){
+        if ( (terrain+((pX-1)+pY*(*largeurTerrain)) )-> contenu != C_BOMBE)
+        (terrain+((pX-1)+pY*(*largeurTerrain)) )->contenu +=1;
+    }
+    if(caseValide(pX-1,pY+1)){
+        if ( (terrain+((pX-1)+(pY+1)*(*largeurTerrain)) )-> contenu != C_BOMBE)
+            (terrain+((pX-1)+(pY+1)*(*largeurTerrain)) )->contenu +=1;
+    }
+    
+}
+
+int caseValide(int pX, int pY)
+/* fonction qui retourne 1 si la case est valide, soit si elle est bien dans le terrain */
+{
+    return ( pX >= 0 && pX < *largeurTerrain && pY >= 0 && pY < *hauteurTerrain);
+}
--- Demineur.h
+++ Demineur.h
@@ -1,8 +1,7 @@
 //
 //  Demineur.h
-//  
 //
-//
+//  Nous avons decide de regouper toutes les fonctions dans un seul header
 //
 
 
@@ -11,32 +10,23 @@
 #define H_DEMINEUR
 
 #define C_VIDE 0
-#define C_BOMBE 1
+#define C_BOMBE 9
 
 #define C_FLAG 2
 #define C_OUVERTE 1
 #define C_FERMEE 0
 
+#define FICH_HAU_LAR ".haular.txt"
 #define FICH_TAB_ENCOURS "terrain.txt"
 #define FICH_TAB_PRECEDENT "terrain_precedent.txt"
-
-#define QUART_HAUT_GAUCHE 0
-#define QUART_BAS_GAUCHE 1
-#define QUART_BAS_DROITE 2
-#define QUART_HAUT_DROITE 3
-#define DEMI_HAUT 4
-#define DEMI_DROITE 5
-#define DEMI_BAS 6
-#define DEMI_GAUCHE 7
-#define TOTAL 8
 
 #define CLEAR_BUFFER while((clearbuffer = getchar()) != '\n' && clearbuffer != EOF )
 
 char clearbuffer;
-int* hauteurTerrain ;//= (int)malloc(sizeof(int)));
-int* largeurTerrain ;//= (int)malloc(sizeof(int)));
+int* hauteurTerrain ;//* création de variable global pour pouvoir continuer une partie *//
+int* largeurTerrain ;//*                                                               *//
 
-typedef struct caseTerrain
+typedef struct caseTerrain //création de la structure dans le header pour quel soit accesible directement par tous
 {
     int posX;
     int posY;
@@ -48,21 +38,31 @@ typedef struct caseTerrain
 void ouvertureCase(caseTerrain* terrain, int pX, int pY);
 void flagCase(caseTerrain* terrain, int pX, int pY);
 int verifCaseMode(int mode, caseTerrain* terrain, int pX, int pY);
-int nbBombeAdjacente(caseTerrain* terrain, int pX, int pY);
+void ouvertureCaseVoisine(caseTerrain* terrain, int pX, int pY);
 int caseValide(int pX, int pY);
+void ajoutNBombe(caseTerrain* terrain, int pX, int pY);
+void plus1CasesVoisines(caseTerrain* terrain, int pX, int pY);
 
 //fonction pour Terrain
 void initTabBombe(int* t, int n, int nbbombe);
 void printTab(int* t, int n);
 void printTerrainTest(caseTerrain* t, int taille);
-void printTerrainJeux(caseTerrain* t);
+int printTerrainJeux(caseTerrain* t);
 void melangeTabBombe(int* t, int n);
 void createTerrain(caseTerrain* terrain, int nbBombe);
-int sauvegardeTerrain(caseTerrain* terrain);
-int afficheTerrainTest(char* fichTab);
+int sauvegardeTerrain(caseTerrain* terrain, char* fichTab);
+int afficheTerrainJeux(char* fichTab);
+void initNbBombe(caseTerrain* terrain);
 caseTerrain* getTerrain(int taille, char* fichTab);
 caseTerrain* ouverture(int cx, int cy);
 caseTerrain* mettreflague(int cx, int cy);
+int sauvegardeHL();
+int getHL();
+void transferTerrain(char* fichACopier, char* fichDestination);
+void printTerrainPerdu(caseTerrain* t);
+int afficheTerrainPerdu(char* fichTab);
+int toutOuvert();
+int toutFlag();
 
 //fonction pour Partie
 void affichageMenu(void);
@@ -72,4 +72,6 @@ void nouvellePartie(void);
 void continuerPartie(void);
 int tour(void);
 void play(void);
+int gagne();
+
 #endif /* fin du bloc délimité par H_DEMINEUR */
